26b3ef81bd33d46818ede35a0cd3a0734c01ff47
jashan <jashandhiman.hulk@gmail.com>
2022-06-13 20:54:35 +0530

    represent data table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import DataStored from "./Components/DataStored";
 
 function App() {
-  return <div className="App">jashan says hello from backend</div>;
+  return (
+    <div className="App">
+      jashan says hello from backend
+      <DataStored />
+    </div>
+  );
 }
 
 export default App;
